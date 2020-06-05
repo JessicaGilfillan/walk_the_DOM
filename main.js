57f7f87f1d6f84a890642ea500f26d52ce4073e4
JessicaGilfillan <49848402+JessicaGilfillan@users.noreply.github.com>
2020-06-05 17:24:18 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,15 @@
+// JavaScript Document
+
+
+//1.) Using ol as your starting element, target the first li element 
+
+
+//2.) using main as your starting element, what is the last child? console.log to check it out 
+
+
+
+//3.) using html as your starting element, what is the last Element child? Console.log to check it out. 
+
+
+
+//4.) using the second li element as your starting element, target the next li element and change the colour to purple. 
